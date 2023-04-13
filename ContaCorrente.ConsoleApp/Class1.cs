504ecc48e3b634c004dfc9297b1e0da8f0cf473f
Joao Marcelo <joaoamarceloalves585@gmail.com>
2023-04-12 23:19:15 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    public class ContaCorrente
+    {
+        public double saldo;
+        public int numero;
+        public double limite;
+        public bool ehsepecial;
+
+        public Movimentacao[] movimentacoes;
+        
+
+        public void Sacar (double quantia)
+        {
+
+            if (quantia < saldo + limite)
+            {
+                double novosaldo = saldo - quantia;
+                 saldo = novosaldo;
+
+                Movimentacao movimentacao = new Movimentacao();
+                movimentacao.valor = quantia;
+                movimentacao.tipo = "Debito";
+                movimentacao.descricao = "Débito de R$" + quantia + "Reais";
+
+                int posicaoVazia = PegaPosicaoVazia();
+
+                movimentacoes[posicaoVazia] = movimentacao;
+
+
+
+
+            }
+
+
+
+        }
+
+        public void Depositar(double quantia)
+        {
+
+            double novosaldo = saldo + quantia;
+
+            saldo = novosaldo;
+
+            Movimentacao.movimentacao = new Movimentacao();
+            movimentacao.valor = quantia;
+            movimentacao.tipo = "Crédito";
+            movimentacao.descricao = "Crédito de R$" + quantia + "Reais";
+
+
+            int pocisaoVazia = PegaPocisaoVazia();
+
+            movimentacoes[posicaoVazia] = PegaPosicaoVazia();
+
+
+
+        }
+
+        public void TransferirPara (ContaCorrente contaDestino,double quantia)
+        {
+
+            this.Sacar(quantia);
+            contaDestino.Depositar(quantia);
+        }
+
+        public void ExibirExtrato()
+        {
+
+            Console.WriteLine("Numero da conta {0}", numero);
+            Console.WriteLine("Movimentações :");
+
+            foreach (Movimentacao  movimentacao in movimentacoes)
+            {
+
+                Console.WriteLine(movimentacao.);
+
+            }
+
+
+        }
+
+
+        public void PegaPosicaoVazia()
+        {
+
+        }
+    }
+
+
+}
---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.saldo = 1000;
+            conta.numero = 12;
+            conta.limite = 0;
+            conta.ehespecial = true;
+            conta.movimentacoes = new Movimentacao[10];
+
+
+            conta.sacar(200);
+
+            conta.depositar(300);
+
+            conta.depositar(500);
+
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.saldo = 1000;
+            conta2.numero = 12;
+            conta2.limite = 0;
+            conta2.ehespecial = true;
+            conta2.movimentacoes = new Movimentacao[10];
+
+            conta.TransferirPara(conta2, 400);
+
+            conta.ExibirExtrato();
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
